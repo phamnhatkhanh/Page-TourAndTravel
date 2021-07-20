bc10237850e7c16c225a5ff592fe159e7eef1c58
PhamNhatKhanh <khanhhcm4@gmail.com>
2021-07-20 07:02:17 +0700

    push code

---
+++ z_rut.js
@@ -0,0 +1,14 @@
+/*
+POSITION:
+*Position-Display:flex;
+ - Position help element at location in body - Flex help format element(in div) center,...
+
+*Selector: when true when false AND way select handmade.
+
+
+
+
+
+
+*/
+
